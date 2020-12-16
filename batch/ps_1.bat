9e08ead470026ce442bf8d4db7713fd5bc1b16db
kermit.yao <kermit.yao@qq.com>
2020-12-16 22:24:36 +0800

    update ps_1.bat add func add powershell execution policy. add auto import from line of powershell

--- batch/ps_1.bat
+++ batch/ps_1.bat
@@ -1,22 +1,91 @@
 @echo off
+setlocal enabledelayedexpansion
+rem ------sys var--------
+set powershellScriptFunc="powershellScriptFunc"
+set powershellScriptFileName=%temp%\pstmp.ps1
+rem ------sys var--------
+
 ver|find "版本 5.">nul&&set sysStatus=True
 if  "%sysStatus%"=="True" (
     echo 不被支持的系统!
     pause
     exit /b 0
 )
-set psfile=%temp%\pstmp.ps1
-(
-for /f "delims= skip=19" %%a in ('type %0') do (
-	echo %%a
+
+call :getUac
+if not "%returnValue%"=="True" (
+    echo 权限不足，请以管理员身份运行!
+    pause
+    exit /b 0
 )
-)>%psfile%
-powershell -file %psfile% %*
-del /q %psfile%
+
+call :getPowershellLine %0
+if "%returnValue%"=="False" (
+    echo 无法找到 powershell 加载点...!
+    pause
+    exit /b 0
+)
+
+(
+for /f "delims= skip=%lineNum%" %%a in ('type %0') do (
+	echo.%%a
+)
+)>%powershellScriptFileName%
+
+if not exist "%powershellScriptFileName%" (
+    echo powershell 脚本加载失败!
+    pause
+    exit /b 0
+)
+
+echo 加载 powershell 脚本...
+call :getExecutionPolicy
+powershell set-executionpolicy -executionpolicy bypass 
+powershell -file %powershellScriptFileName% %*
+powershell set-ExecutionPolicy -executionpolicy %executionPolicy%
+::del /q %powershellScriptFileName%
+pause
 exit /b 0
 
+rem 获取UAC状态;return=Null|False|True
+:getUac
+set uacStatus=
+set returnValue=
+(echo u >%windir%\u.tmp)2>nul
+if not exist %windir%\u.tmp (
+	set uacStatus=False
+) else (
+	set uacStatus=True
+	del /f %windir%\u.tmp 2>nul
+)
 
-:ps1
+if "#"=="#!uacStatus!" (
+	set returnValue=Null
+) else (
+	set returnValue=%uacStatus%
+)
+goto :eof
+
+:getPowershellLine
+for /f "delims=" %%a in ('type %1 ^| findstr /n ":%powershellScriptFunc%"') do (
+	if not "%%a"=="" (
+		for /f "delims=: tokens=1*" %%x in ("%%a") do (
+			set lineNum=%%x
+		)
+	) else (
+		set lineNum=False
+	)
+)
+goto :eof
+
+:getExecutionPolicy
+for /f "delims=" %%a in ('powershell get-ExecutionPolicy -scope LocalMachine') do (
+	set executionPolicy="%%a"
+)
+goto :eof
+
+
+:powershellScriptFunc
 $pros = (Get-Process)
 $prosMax = 20
 write-host "$args"
@@ -24,3 +93,4 @@ for($n=1;$prosmax -gt $n;$n++)
 {
     echo $n
 }
+
