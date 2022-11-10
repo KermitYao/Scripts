1547feb8ebae90ab058335842a0d635b6aec3943
kermit yao <kermit.yao@qq.com>
2022-11-10 11:03:35 +0800

    update somethings file name for bat and sh.

--- batch/360Inst.bat
+++ batch/360Inst.bat
@@ -1,12 +1,20 @@
 1>1/* ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 @echo off
-title 360Install tool.
+title 360Inst tool.
 setlocal enabledelayedexpansion
-set sdUrl=http://192.168.16.196:8081/online/Ent_360EPP1383860355[192.168.16.196-8084]-W.exe
+set sdUrl=http://360epp.yjyn.top:8081/online/Ent_360EPP1383860355[360epp.yjyn.top-8084]-W.exe
 
 rem 临时文件和日志存放路径
 set path_Temp=%temp%\360install
-set name_360=Ent_360EPP1383860355[192.168.16.196-8084]-W.exe
+
+rem 用于域控推送的时候静默运行 True|False
+set quiet=False
+
+if "%~1"=="/q" set quiet=True
+for /f "delims=/ tokens=4" %%a in ("%sdUrl%") do (
+	echo %%a|findstr "^Ent_360EPP[0-9]*\[.*\]-W.exe" >nul
+	if !errorlevel! equ 0 set name_360=%%a
+)
 if not exist %path_Temp% md %path_Temp%
 
 rem 用于启动第三方杀毒软件卸载程序,本质是搜索注册表键值,如果存在相应的键值,则启动卸载程序
@@ -21,6 +29,20 @@ set argsAvUninst=True
 set args360Inst=True
 echo 正在处理相关信息...
 
+rem 判断epp是否安装
+for %%a in (%registryKey%) do (
+	for /f "tokens=1-2*" %%e in ('reg query "%%~a\360EPPX" /v %registryValue% 2^>nul') do (
+		if not "%%~g"=="" (
+			set eppFlag=True
+			set eppPath=%%~g
+		)
+	)
+)
+if "%eppFlag%"=="True" (
+	echo 检测到已经安装EPP,无需再次安装.
+	set exitCode=0
+	goto :close
+)
 rem 卸载第三方安全软件
 if "#%argsAvUninst%"=="#True" (
 	echo 开始扫描第三方安全软件...
@@ -41,19 +63,30 @@ if "#%args360Inst%"=="#True" (
 	if "#!returnValue!"=="#True" (
 		if exist "%path_Temp%\%name_360%" (
 			echo 启动360安装程序...
-			start /b "360Install" "%path_Temp%\%name_360%"
-			exit /b 0
+			if "%quiet%"=="True" (
+				start /b "360Install" "%path_Temp%\%name_360%" /s
+				set exitCode=0
+			) else (
+				start /b "360Install" "%path_Temp%\%name_360%"
+				set exitCode=0
+			)
+
 		) else (
 			echo 未知错误,无法启动360安装程序.
-			exit /b 99
+			set exitCode=99
 		)
 	) else (
 		echo 360安装文件下载失败.
-		exit /b 1
+		set exitCode=1
 	)
 )
 
-exit /b 0
+:close
+if not "%quiet%"=="True" (
+		echo 脚本已完成,按任意键退出.
+		pause >nul
+)
+exit /b %exitCode%
 
 rem 卸载第三方杀毒软件
 :avUninst
@@ -79,14 +112,12 @@ rem 下载文件; 传入参数: %1 = 当前文件路径， %2 = url, %3 = 保存地址; 例：call :d
 set downStatus=False
 for  /f %%a in  ('cscript /nologo /e:jscript "%~f1" /downUrl:%2 /savePath:%3') do (
 	if "#%%a"=="#True" set downStatus=False
-	echo The file [%~2] was download by "jscript"
 )
 
 if "#!downStatus!"=="#False" (
 	if not "#%sysVersion%"=="#WindowsXp" (
 		for  /f "delims=" %%a in  ('powershell -Command "& {(New-Object Net.WebClient).DownloadFile('%~2', '%~3');($?)}" 2^>nul') do (
 				if "#%%a"=="#True" set downStatus=True
-				echo The file [%~2] was download by "powershell"
 		)
 	)
 )
