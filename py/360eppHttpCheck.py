8b0012df68269ab162f2f9b74e58eb06633dcd21
13262951674 <nameyu8023@qq.com>
2023-05-25 13:26:52 +0800

    add 360eppHtppCheck.py and fileBackupBySSH.py

---
+++ py/360eppHttpCheck.py
@@ -0,0 +1,104 @@
+import requests
+from datetime import datetime
+from email import encoders
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr, formataddr
+import smtplib
+import os,sys,time
+#格式化时间
+def formatTime(times,type=1):
+    if times != None:
+        m=time.localtime(float(times))
+        tm={'year':m.tm_year,
+            'mon':m.tm_mon,
+            'mday':m.tm_mday,
+            'hour':m.tm_hour,
+            'min':m.tm_min,
+            'sec':m.tm_sec,
+        }
+        for v in tm:
+            if tm[v]<10:
+                tm[v]='0'+str(tm[v])
+            else:
+                tm[v]=str(tm[v])
+        if type == 1:
+            m='{}-{}-{} {}:{}:{}'.format(tm['year'],tm['mon'],tm['mday'],tm['hour'],tm['min'],tm['sec'])
+        elif type == 2:
+            m='{}{}{}{}{}{}'.format(tm['year'],tm['mon'],tm['mday'],tm['hour'],tm['min'],tm['sec'])
+        elif type == 3:
+            m='{}-{}-{}'.format(tm['year'],tm['mon'],tm['mday'])
+        return m
+    return None
+
+
+def logWrite(errorType, codeLine, message):
+    logLevel = 'INFO'
+    logPath = __file__+'.log'
+
+    if logLevel == 'DEBUG':
+        logLevelList = ['DEBUG', 'INFO', 'WARNING', 'ERROR']
+    elif logLevel == 'INFO':
+        logLevelList = ['INFO', 'WARNING', 'ERROR']
+    elif logLevel == 'WARNING':
+        logLevelList = ['WARNING', 'ERROR']
+    elif logLevel == 'ERROR':
+        logLevelList = ['ERROR',]
+    else:
+        logLevelList = ['DEBUG', 'INFO', 'WARNING', 'ERROR']
+
+    for item in logLevelList:
+        if item == errorType:
+            msg = '{0} -- {1} -- line:{2} -- {3}'.format(formatTime(time.time()), errorType, codeLine, message)
+            print(msg)
+            with open(logPath, 'a+') as f:
+                f.write(msg+'\n')
+
+def sendMail(from_addr, password, to_addr, smtp_server, form_msg): 
+    def _format_addr(send_format):
+        name, addr = parseaddr(send_format)
+        return formataddr((Header(name, 'utf-8').encode(), addr))
+    msg = MIMEText(form_msg, 'plain', 'utf-8')
+    msg['From'] = _format_addr('Python robot <%s>' % from_addr)
+    msg['To'] = _format_addr('NGRAIN <%s>' % to_addr)
+    if len(ccList) > 0:
+        msg['Cc'] = _format_addr(ccList)
+    msg['Subject'] = Header('EPP 服务器存活监测', 'utf-8').encode()
+    try:
+        server = smtplib.SMTP_SSL(smtp_server, 465)
+        #server.set_debuglevel(1)
+        server.login(from_addr, password)
+        server.sendmail(from_addr, [to_addr]+ccList, msg.as_string())
+        server.quit()
+        return True
+    except smtplib.SMTPException as e:
+        logWrite('ERROR', sys._getframe().f_lineno, '邮件发送错误报告: %s' % e)
+        return False
+
+def urlTest(url):
+    logWrite('INFO', sys._getframe().f_lineno, '测试服务器:%s 是否存活' % url)
+    try:
+        r = requests.get(url,timeout=5)
+        if r.status_code == 200:
+            return True
+        else:
+            return False
+    except:
+        return False
+
+def main():
+    for url in urlList:
+        if urlTest(url):
+            logWrite('INFO', sys._getframe().f_lineno, '服务器:%s 运行正常' % url)
+        else:
+            logWrite('WARNING', sys._getframe().f_lineno, '服务器:%s 运行异常' % url)
+            if sendMail('714580117@qq.com', '1jfbyhvwwtaxrbefj', 'kermit.yao@qq.com', 'smtp.qq.com',url+' 无法访问,请尽快处理'):
+                logWrite('INFO', sys._getframe().f_lineno, '邮件发送正常')
+            else:
+                logWrite('WARNING', sys._getframe().f_lineno, '邮件发送异常')
+    return True
+
+if __name__ == '__main__':
+    urlList = ["http://10.152.9.185:8081", "http://10.152.9.220:8081", "http://10.152.10.97:8081"]
+    ccList = ["170374690@qq.com"]
+    main()
---
+++ py/fileBackupBySSH.py
@@ -0,0 +1,134 @@
+#!/usr/bin/python3
+#Backup files to remote server
+from genericpath import isfile
+from posixpath import relpath
+import paramiko,os,sys,time
+import socket
+
+class SSHTools():
+    def __init__(self):
+        self.hostname=None
+        self.port=22
+        self.username=None
+        self.password=None
+        self.timeout=10
+
+    def connect(self):
+        if self.hostname == None or self.port == None or self.username == None or self.password == None:
+            logWrite('WARNING', sys._getframe().f_lineno, '参数不完整')
+            return False
+    
+        self.sshClient = paramiko.SSHClient()
+        self.sshClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            self.sshClient.connect(hostname=self.hostname, port=self.port, username=self.username, password=self.password, timeout=self.timeout)
+            self.sftp = self.sshClient.open_sftp()
+            return True
+        except:
+            logWrite('WARNING', sys._getframe().f_lineno, '主机连接失败：%s' % self.hostname)
+            return False
+    def exec(self,cmd):
+        stdin, stdout, stderr = self.sshClient.exec_command(cmd)
+        return stdout.read().decode('utf-8'),stderr.read().decode('utf-8')
+
+    def ls(self,dir):
+        return self.sftp.listdir(dir)
+    
+    def isFile(self,path):
+        if self.exec('test -f %s&&printf True' % path)[0] == "True":
+            return True
+        return False
+
+    def isDir(self,path):
+        if self.exec('test -d %s&&printf True' % path)[0] == "True":
+            return True
+        return False
+
+    def mkdir(self,path):
+        if self.exec('mkdir -p %s&&printf True' % path)[0] == "True":
+            return True
+        return False
+
+    def put(self,local,remote):
+        return self.sftp.put(local,remote)
+    
+    def close(self):
+        return self.sshClient.close()
+
+#格式化时间
+def formatTime(times,type=1):
+    if times != None:
+        m=time.localtime(float(times))
+        tm={'year':m.tm_year,
+            'mon':m.tm_mon,
+            'mday':m.tm_mday,
+            'hour':m.tm_hour,
+            'min':m.tm_min,
+            'sec':m.tm_sec,
+        }
+        for v in tm:
+            if tm[v]<10:
+                tm[v]='0'+str(tm[v])
+            else:
+                tm[v]=str(tm[v])
+        if type == 1:
+            m='{}-{}-{} {}:{}:{}'.format(tm['year'],tm['mon'],tm['mday'],tm['hour'],tm['min'],tm['sec'])
+        elif type == 2:
+            m='{}{}{}{}{}{}'.format(tm['year'],tm['mon'],tm['mday'],tm['hour'],tm['min'],tm['sec'])
+        elif type == 3:
+            m='{}-{}-{}'.format(tm['year'],tm['mon'],tm['mday'])
+        return m
+    return None
+
+
+def logWrite(errorType, codeLine, message):
+    logLevel = 'INFO'
+    logPath = __file__+'.log'
+
+    if logLevel == 'DEBUG':
+        logLevelList = ['DEBUG', 'INFO', 'WARNING', 'ERROR']
+    elif logLevel == 'INFO':
+        logLevelList = ['INFO', 'WARNING', 'ERROR']
+    elif logLevel == 'WARNING':
+        logLevelList = ['WARNING', 'ERROR']
+    elif logLevel == 'ERROR':
+        logLevelList = ['ERROR',]
+    else:
+        logLevelList = ['DEBUG', 'INFO', 'WARNING', 'ERROR']
+
+    for item in logLevelList:
+        if item == errorType:
+            msg = '{0} -- {1} -- line:{2} -- {3}'.format(formatTime(time.time()), errorType, codeLine, message)
+            print(msg)
+            with open(logPath, 'a+') as f:
+                f.write(msg+'\n')
+
+def main():
+    localhostname=socket.gethostname()
+    remoteBackupDir='/data/backup/%s/' % localhostname
+    localBackupDir='/home/360data/docker/cactus-web/system/data.restore/'
+    sshTools = SSHTools()
+    sshTools.hostname = '10.152.9.185'
+    sshTools.username = 'root'
+    sshTools.password = 'FXEDU_2023'
+    if sshTools.connect():
+        if not sshTools.isDir(remoteBackupDir):
+            sshTools.exec('mkdir -p %s' % remoteBackupDir)
+        for r,d,f in os.walk(localBackupDir):
+            relPath = r.replace(localBackupDir, '')
+            for name in d:
+                remoteDir = os.path.join(remoteBackupDir,relPath,name).replace('\\','/')
+                if not sshTools.isDir(remoteDir):
+                    logWrite('INFO', sys._getframe().f_lineno, '创建远程目录：%s' % remoteDir)
+                    sshTools.mkdir(remoteDir)
+
+            for name in f:
+                localFile = os.path.join(r,name).replace('/',os.sep)
+                remoteFile = os.path.join(remoteBackupDir,relPath, name).replace('\\','/')
+                if not sshTools.isFile(remoteFile):
+                    logWrite('INFO', sys._getframe().f_lineno, '开始上传文件：%s' % localFile)
+                    sshTools.put(localFile, remoteFile)
+        return sshTools.close()
+    
+if '__main__' == __name__:
+    main()
