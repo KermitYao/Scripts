da635fef15a3e204cca72d56106249f8866c6bfe
kermit yao <kermit.yao@qq.com>
2023-08-14 19:46:11 +0800

    update eppapi.py

--- py/eppapi.py
+++ py/eppapi.py
@@ -1,107 +1,229 @@
-#-*-coding:utf-8-*-
-import requests
-import time,string,random,hashlib
-from urllib.parse import urljoin
+
+#pip install pycryptodomex
+#pip install requests
+
 import base64
-#当系统为 nt 时安装: pycryptodome, linux为: pycrypto
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad
-class GetEppData():
-    def __init__(self):
-        self.appInfo = ''
-        self.tokenInfo = ''
-        self.url = ''
-        self.urlHost = ''
-        self.randomStr = ''.join(random.sample(string.ascii_letters + string.digits + string.punctuation, 32))
+import hashlib
+import random
+import time
+from Cryptodome.Cipher import AES
+import requests
 
-    #初始化token
-    def initToken(self,appInfo,urlHost):
-        intStr = (appInfo['appKey'] + self.randomStr + appInfo['appSecret'] + str(int(time.time()))).encode("utf-8")
-        md5Str = hashlib.md5(intStr).hexdigest()
-        data = {
-        'app_name': appInfo['appName'],
-        'app_key': appInfo['appKey'],
-        'sign': md5Str,
-        'nonce': self.randomStr,
-        'stime': str(int(time.time())),
+class EppApi():
+    def __init__(self, appName, appKey, appSecret, iv, host):
+        self.appKey = appKey
+        self.appSecret = appSecret
+        self.appName = appName
+        self.iv = iv
+        self.host = host
+        self.accessToken = ""
+        self.tokenExpires = 0
+    def getToken(self):
+        if int(time.time()) >= self.tokenExpires:
+            url = f"{self.host}/openapi/statistics/gettoken"
+            nonce = ''.join(random.choice('0123456789ABCDEF') for i in range(32))
+            stime = int(time.time())
+            joinstr = f"{self.appKey}{nonce}{self.appSecret}{stime}"
+            sign = hashlib.md5(joinstr.encode(encoding='utf-8')).hexdigest()
+            params = {
+                "app_key": self.appKey,
+                "app_name": self.appName,
+                "stime": stime,
+                "nonce": nonce,
+                "sign": sign
+            }
+            content = requests.post(url, data=params)
+            response = content.json()
+            self.accessToken = response['data']["accessToken"]
+            self.tokenExpires = int(time.time()) + response['data']["expiresIn"]
+
+    def decodeData(self, data):
+        data = base64.b64decode(data)
+        data = data[4:]
+        bAppSecret = bytes(self.appSecret[0:32], encoding="utf-8")
+        bIv = bytes(self.iv, encoding="utf-8")
+        cipher = AES.new(bAppSecret, AES.MODE_CBC, bIv)
+        msg = cipher.decrypt(data)
+        padding_len = msg[len(msg) - 1]
+        return msg[0:-padding_len]
+    
+    #获取每个分组下的客户端安装数量
+    def getGroupTree(self):
+        url = f"{self.host}/openapi/statistics/getmygrouptree"
+        self.getToken()
+        params = {
+            "token": self.accessToken
         }
-        urlPath = 'openapi/statistics/gettoken'
-        url = urljoin(urlHost, urlPath)
-        r = requests.post(url, data=data)
-        if r.status_code != 200:
-            return False
+        content = requests.post(url, data=params)
+        response = content.json()
+        if response['errno'] == 0:
+            data = response['data']
+            return self.decodeData(data)
         else:
-            if r.json()['errno'] != 0:
-                return False
-            else:
-                self.tokenInfo = r.json()
-                self.appInfo = appInfo
-                self.urlHost = urlHost
-                return True
-        return False
+            return response['errmsg']
 
-    def __checkToken(self):
-        if self.tokenInfo:
-            return True
+    #获取操作系统统计
+    def getPcInfo(self):
+        url = f"{self.host}/openapi/statistics/pcinfo"
+        self.getToken()
+        params = {
+            "token": self.accessToken
+        }
+        content = requests.post(url, data=params)
+        response = content.json()
+        if response['errno'] == 0:
+            data = response['data']
+            return self.decodeData(data)
         else:
-            print('Token 信息未获取!')
-            return False
+            return response['errmsg']
 
-    #解析数据
-    def __deData(self,data):
-        print(data)
-        print('')
-        deData = base64.b64decode(data['data'])
-        deData = deData[4:]
-        appkey = self.appInfo['appKey'].encode()
-        if len(self.appInfo['iv']) % 16 != 0:
-            iv = pad(self.appInfo['iv'].encode(), 16)
+    #资产统计
+    def getAssetstat(self):
+        url = f"{self.host}/openapi/statistics/assetstat"
+        self.getToken()
+        params = {
+            "token": self.accessToken
+        }
+        content = requests.post(url, data=params)
+        response = content.json()
+        if response['errno'] == 0:
+            data = response['data']
+            return self.decodeData(data)
         else:
-            iv = self.appInfo['iv'].encode()
-        aes = AES.new(appkey,AES.MODE_CBC,iv)
-        deData = aes.decrypt(deData)
+            return response['errmsg']
+
+    #客户端版本统计
+    def geteppVersion(self):
+        url = f"{self.host}/openapi/statistics/eppversion"
+        self.getToken()
+        params = {
+            "token": self.accessToken
+        }
+        content = requests.post(url, data=params)
+        response = content.json()
+        if response['errno'] == 0:
+            data = response['data']
+            return self.decodeData(data)
+        else:
+            return response['errmsg']
+
+    #病毒库版本统计
+    def getVirusLibVersion(self):
+        url = f"{self.host}/openapi/statistics/virus"
+        self.getToken()
+        params = {
+            "token": self.accessToken
+        }
+        content = requests.post(url, data=params)
+        response = content.json()
+        if response['errno'] == 0:
+            data = response['data']
+            return self.decodeData(data)
+        else:
+            return response['errmsg']
+
+    #补丁库版本统计
+    def getPatchLibVersion(self):
+        url = f"{self.host}/openapi/statistics/patchlibversion"
+        self.getToken()
+        params = {
+            "token": self.accessToken
+        }
+        content = requests.post(url, data=params)
+        response = content.json()
+        if response['errno'] == 0:
+            data = response['data']
+            return self.decodeData(data)
+        else:
+            return response['errmsg']
+
+    def getGroupList(self):
+        url = f"{self.host}/openapi/statistics/violations"
+        self.getToken()
+        params = {
+            "token": self.accessToken
+        }
+        content = requests.post(url, data=params)
+        response = content.json()
+        if response['errno'] == 0:
+            data = response['data']
+            return self.decodeData(data)
+        else:
+            return response['errmsg']
         
-        return deData[1:]
-    #获取组信息
-    def getData(self):
-        if self.__checkToken() == False:
-            return False
-        urlPath = '/openapi/statistics/virus'
-        url = urljoin(self.urlHost, urlPath)
-        data = {
-            'token': self.tokenInfo['data']['accessToken'],
+    def scanFast(self,m2,handleType="auto",expireTime=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()+172800))):
+        url = f"{self.host}/openapi/SetCommand/scanfast"
+        self.getToken()
+        params = {
+            "token": self.accessToken,
+            "m2s":m2,
+            "handle_type":handleType,
+            "expire_time":expireTime
         }
-        r = requests.post(url, data=data)
-        if r.status_code != 200:
-            return False
+        content = requests.post(url, data=params)
+        response = content.json()
+        if response['errno'] == 0:
+            data = response['data']
+            return self.decodeData(data)
         else:
-            if r.json()['errno'] != 0:
-                return False
-            else:
-                return self.__deData(r.json())
+            return response['errmsg']
+
+    def updateVirus(self,m2):
+        url = f"{self.host}/openapi/SetCommand/clientupdatevirus"
+        self.getToken()
+        params = {
+            "token": self.accessToken,
+            "m2s":m2,
+        }
+        content = requests.post(url, data=params)
+        response = content.json()
+        if response['errno'] == 0:
+            data = response['data']
+            return self.decodeData(data)
+        else:
+            return response['errmsg']
+
+    def updatePatch(self,m2):
+        url = f"{self.host}/openapi/SetCommand/clientupdatepatch"
+        self.getToken()
+        params = {
+            "token": self.accessToken,
+            "m2s":m2,
+        }
+        content = requests.post(url, data=params)
+        response = content.json()
+        if response['errno'] == 0:
+            data = response['data']
+            return self.decodeData(data)
+        else:
+            return response['errmsg']
+        
+    def scanFull(self,m2,handleType="auto",expireTime=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()+172800))):
+        url = f"{self.host}/openapi/SetCommand/scanfast"
+        self.getToken()
+        params = {
+            "token": self.accessToken,
+            "m2s":m2,
+            "handle_type":handleType,
+            "expire_time":expireTime
+        }
+        content = requests.post(url, data=params)
+        response = content.json()
+        if response['errno'] == 0:
+            data = response['data']
+            return self.decodeData(data)
+        else:
+            return response['errmsg']
 
 
-        return r.json()
+if __name__ == '__main__':
+    appName = "test"
+    appKey = "a5fc390c5d9701395131235b9b425f50"
+    appSecret = "274070d5b2ff11da9cf94e0651cc1f627fdc1eee"
+    iv = "15K55c7vYwZ657Gl"
+    host = "http://192.168.16.196:8081"
 
-
-def main():
-    appInfo = {
-        'appName': 'pytest',
-        'appKey': '11d7825afa15ed2ffec848ff0e3e210e',
-        'appSecret': '4f97ae80a56bf3800527eac026ebe66d5041c504',
-        'iv': '2EBPJd76eb000000'
-    }
-    urlHost = 'http://192.168.16.196:8081'
-    app = GetEppData()
-    tokenState = app.initToken(appInfo,urlHost)
-    if tokenState :
-        print('Get token okey!')
-        s = app.getData()
-        with open('s.text',"wb+") as f:
-            f.write(s)
-        return  tokenState
-    else:
-        print('Get token failed')
-        return  tokenState
-if __name__=='__main__':
-    main()
+    api = EppApi(appName, appKey, appSecret, iv, host)
+    print(api.getGroupTree())
+    print(api.getPcInfo())
+    print(api.geteppVersion())
